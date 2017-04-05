905b4cb3766363b26a1171b3b4cfb822c253ac0a
Cliff-Hsieh <qm29213039@gmail.com>
2017-04-05 17:10:49 +0800

    feat(spider): crawl data

--- build/lib/players/spiders/player.py
+++ build/lib/players/spiders/player.py
@@ -10,9 +10,18 @@ class PlayerSpider(scrapy.Spider):
 
     def parse(self, response):
         yield Request("http://sports.yahoo.com/mlb/players/9558/", callback=self.parse_detail_page)
-    
+
     def parse_detail_page(self, response):
         item = playerProp()
+        item['name'] = response.xpath('//span[@data-reactid="10"]/text()').extract()
+        item['team'] = response.xpath('//a[@data-reactid="22"]/text()').extract()
+        item['num'] = response.xpath('//span[@data-reactid="16"]/text()').extract()
+        item['pos'] = response.xpath('//span[@data-reactid="18"]/text()').extract()
+        item['avg'] = response.xpath('//div[@data-reactid="26"]/text()').extract()
+        item['hr'] = response.xpath('//div[@data-reactid="29"]/text()').extract()
+        item['rbi'] = response.xpath('//div[@data-reactid="32"]/text()').extract()
+        item['run'] = response.xpath('//div[@data-reactid="35"]/text()').extract()
+
         yield item
 
 
